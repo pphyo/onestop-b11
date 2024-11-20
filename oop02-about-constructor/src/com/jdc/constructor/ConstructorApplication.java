097e747f311a9ddc77569888f16632aadc097d1f
pphyo <pyaephyo.jdc@gmail.com>
2024-11-20 11:50:04 +0630

    Explanation: Ensapsulation with validation in setter

--- oop02-about-constructor/src/com/jdc/constructor/ConstructorApplication.java
+++ oop02-about-constructor/src/com/jdc/constructor/ConstructorApplication.java
@@ -6,12 +6,29 @@ public class ConstructorApplication {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter age: ");
-		int age = sc.nextInt();
 
 		Person person = new Person("U Tin Mg");
+
+		System.out.print("Enter name: ");
+		String name = sc.nextLine();
+		person.setName(name);
+
+		System.out.print("Enter age: ");
+		int age = Integer.parseInt(sc.nextLine());
 		person.setAge(age);
-		System.out.println("Age: ");
+
+		System.out.print("Enter phone: ");
+		String phone = sc.nextLine();
+		person.setPhone(phone);
+
+		System.out.println("""
+							Name : %s
+							Age  : %d
+							Phone: %s""".formatted(person.getName(), 
+												   person.getAge(),
+												   person.getPhone()
+												   )
+						  );
 
 		sc.close();
 	}
--- oop02-about-constructor/src/com/jdc/constructor/Person.java
+++ oop02-about-constructor/src/com/jdc/constructor/Person.java
@@ -2,15 +2,69 @@ package com.jdc.constructor;
 
 class Person {
 
+	// must "a-z||A-Z", must not "   ", "", "12"
 	private String name;
-	private int age;
+	private int age = 1;
+	// must "+959", "09", length 9
 	private String phone;
+//	name = ""; // compile error
 
+	void setName(String input) {
+		// ""
+		char first = input.length() == 0 ? '\u0000' : input.charAt(0);
+
+		boolean isSmall = first >= 65 && first <= 90;
+		boolean isCapital = first >= 97 && first <= 122;
+
+		if(!(isSmall || isCapital)) {
+			System.out.println("Please enter correct name!");
+		} else {
+			name = input;
+		}
+	}
+
+	String getName() {
+		return name;
+	}
+
+	// setter or mutator
 	void setAge(int input) {
 		if(input <= 0) {
 			System.err.println("Age must be greater than zero.");
+		} else {
+			age = input;
 		}
-		age = input;
+	}
+
+	// getter or accessor
+	int getAge() {
+		return age;
+	}
+
+	void setPhone(String input) {
+		String prefixOne = "+959";
+		String prefixTwo = "09";
+
+		boolean result = false;
+
+		if(input.startsWith(prefixOne) || input.startsWith(prefixTwo)) {
+			if(input.startsWith(prefixOne)) {
+				result = input.substring(4).length() == 9 ? true : false;
+			} else {
+				result = input.substring(2).length() == 9 ? true : false;
+			}
+		}
+
+		if(result) {
+			phone = input;
+		} else {
+			System.out.println("Your mobile number is incorrect!");
+		}
+
+	}
+
+	String getPhone() {
+		return phone;
 	}
 
 	// default constructor => constructor with no argument
